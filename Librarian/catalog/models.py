7928358f9aabd15cbf4a3d6c8b9bd88735bc82e8
Enricima <mathis.enrici@gmail.com>
2022-12-08 16:51:25 +0100

    1er modèle créé

--- Librarian/catalog/models.py
+++ Librarian/catalog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+
+    #Variables
+    LANGUAGE = [('EN', 'English'), ('FR', 'French'), ('DE', 'German'), ('ES', 'Spanish'), ('IT', 'Italian')]
+
+    #Champs
+    ISBN = models.CharField(max_length=25)
+    name = models.CharField(max_length=50)
+    subject = models.TextField()
+    overview = models.TextField()
+    publisher = models.CharField(max_length=50)
+    publicationDate = models.DateField(auto_now_add=True)
+    lang = models.CharField(max_length=50, choices=LANGUAGE)
+
+    #Métadata (ordonnancement)
+    class Meta:
+        ordering = ['name']
+
+    # Méthodes
+    def __str__(self):
+        """Représentation de MyModelName (Admin site etc.)."""
+        return self.ISBN
+
+
+
+
